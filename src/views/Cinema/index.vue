ef23bd15da895dec6b0811321c68157a1fc90ff4
xuxiang <276378532@qq.com>
2019-07-23 10:14:10 +0800

    static router

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,47 @@
+<template>
+   <div id='main'>
+        <movieHeader title="喵喵影院"></movieHeader>
+         <div id="content">
+            <div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+            <cinemaList></cinemaList>
+        </div>
+        <movie-tab-bar></movie-tab-bar> 
+        cinema
+    </div>
+</template>
+
+<script>
+import movieHeader from "@/components/Header";
+import movieTabBar from "@/components/TabBar";
+import cinemaList from "@/components/CinemaList";
+export default {
+  name: "Cinema",
+  components: {
+    movieHeader,
+    movieTabBar,
+    cinemaList
+  }
+};
+</script>
+
+<style scoped>
+#content .cinema_menu {
+  width: 100%;
+  height: 45px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: white;
+}
+</style>
